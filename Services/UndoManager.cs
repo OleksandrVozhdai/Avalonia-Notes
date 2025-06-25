73d18cfea7a63a0d2a46e5805a8b41da3e5f827e
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-06-25 13:50:13 +0300

    Add undo functionality for text formatting actions

---
+++ Services/UndoManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotepad.Services
+{
+	/// <summary>
+	/// Manages a stack of undoable actions, allowing actions to be added and undone.
+	/// </summary>
+	internal class UndoManager
+	{
+		private Stack<IUndoableAction> _undoStack = new Stack<IUndoableAction>();
+
+		public void AddAction(IUndoableAction action)
+		{
+			_undoStack.Push(action);
+		}
+
+		public void UndoLast()
+		{
+			if (_undoStack.Any())
+			{
+				_undoStack.Pop().Undo();
+			}
+		}
+	}
+}
